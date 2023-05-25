3dab28dec8b26afe7ac4a646012e634697cda978
bat-kin <134644147+bat-kin@users.noreply.github.com>
2023-05-25 15:12:42 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+    {
+        MessageBack.Text = $"Swiped {e.Direction.ToString().ToUpper()}. Try again.";
+        BotImage.Rotation += 90;
+    }
+}
+
